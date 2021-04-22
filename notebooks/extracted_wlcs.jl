621e3b0d144b370b6f2032119d61ef5bc734d59b
icweaver <iweaver@cfa.harvard.edu>
2021-04-22 18:07:22 -0400

    switched to makie

--- notebooks/extracted_wlcs.jl
+++ notebooks/extracted_wlcs.jl
@@ -15,13 +15,11 @@ end
 
 # ╔═╡ b1b0690a-a1eb-11eb-1590-396d92c80c23
 begin
-	using PlutoUI
-	using Statistics#, StatsPlots
+	import PlutoUI as pl
+	using CairoMakie
+	using Statistics
 	using PyCall
 end
-
-# ╔═╡ 36e5bb5f-75a1-47c3-baa3-66bd77eb9515
-using Gadfly
 
 # ╔═╡ 2d53ee50-c95b-4e9c-9ddb-e47ed9e3168b
 md"""
@@ -30,9 +28,9 @@ md"""
 
 # ╔═╡ 1293b7f7-3054-4504-965f-2bd8ebe05e9e
 md"""
-$(TableOfContents())
+$(pl.TableOfContents())
 
-In this notebook we will visualize the white-light and wavelength binned, divided light curves from the raw flux extracted from LDSS3 using [(cite)]().
+In this notebook we will visualize the stellar spectra, white-light, and wavelength binned light curves from the raw flux extracted from LDSS3 using [(cite)]().
 """
 
 # ╔═╡ 470c738f-c2c8-4f56-b936-e08c43c161b1
@@ -50,21 +48,26 @@ This is an `npy` file that contains the following fields:
 * spectral
     * Dict with two entries:
         * `wavelength`: Array of wavelengths in final grid, e.g., 5000-9799, with length ``W``
-        * `fractionofapixel`: **Same length array, but not sure what each value here represents?**
+        * `fractionofapixel`: Amount of each pixel per wavelength bin
 * comparisons
     * Similar to `stellar`, but a list of the comparison star square names only
 * target
     * Similar to `stellar`, but a list of the target star square name(s) only
 * cubes
-    * Dict with keys: `raw_counts`, `peak`, `ok`, `sky`, `width`, and `centroid`. Each key contains an ``N \times W`` matrix of measurements, 1 matrix for each square. **I was wondering if the definition for each key is stored somewhere online?**
+    * Dict with keys: `raw_counts`, `peak`, `ok`, `sky`, `width`, and `centroid`. Each key contains an ``N \times W`` matrix of measurements, 1 matrix for each square.
 
 !!! note
-	We will use `wavelength` and `raw_counts` for our analysis here, where `raw_counts` is the sky subtracted, cross-dispersion integrated flux from each star.))
+	We will use `wavelength` and `raw_counts` for our analysis here, where `raw_counts` is the sky subtracted, cross-dispersion integrated flux from each star.
+"""
+
+# ╔═╡ 4dddc586-2383-41f8-a888-ef421372d71a
+md"""
+The contents of each field can also be interactively explored using the tree viewer below:
 """
 
 # ╔═╡ 7185f603-3e57-42db-9665-c215094776ad
 md"""
-Let's extract the target and comparison star flux and compute the resulting spectrum and light curves:
+Next we extract the common wavelength grid, along with the target and comparison star flux, and compute the resulting spectrum and light curves:
 """
 
 # ╔═╡ 7bfc971c-8737-49ad-adec-ac57d176f10e
@@ -74,12 +77,22 @@ We can extract the comparison star flux in a similar way by stacking the ``N \ti
 
 # ╔═╡ 968d1f11-12a0-4b8f-abb5-0195652e4e1f
 md"""
-With the releavnt fields extracted, we can now explore the various data products below:
+With the relevant fields extracted, we can now explore the various data products below.
 """
 
 # ╔═╡ e774a20f-2d58-486a-ab71-6bde678b26f8
 md"""
 ## Stellar spectra 🌟
+"""
+
+# ╔═╡ ed92fff0-6413-4fc2-939e-ecd13430ce75
+md"""
+This is the extracted flux from each star, as a function of wavelength.
+"""
+
+# ╔═╡ 843fc32b-a0c8-4281-876a-9579a862548b
+md"""
+### Plot
 """
 
 # ╔═╡ d6c17e99-452e-43e0-b998-10cb81009076
@@ -92,13 +105,25 @@ md"""
 ## White light curves ⚪
 """
 
-# ╔═╡ 1632c9d9-d61f-4bf6-bbfa-4d5f7deee5f3
-let
-	p1 = plot(rand(100))
-	p2 = plot(rand(100))
-	
-	testplot = plot(p1, p2, link=:both, ylabel="Y", layout=(2,1))
-end
+# ╔═╡ 08eafc08-b0fb-4c99-9d4e-7fa5085d386c
+md"""
+With these spectra now extracted, we can integrate the flux along the spectral direction to build the white-light curves for each star. This will be stored in the `ntimes` ``\times`` 1 ``\times`` `ncomps` array, `f_norm_wlc`.
+"""
+
+# ╔═╡ ae39e476-3850-4bbf-aa45-0a16c2425324
+md"""
+### Integrate spectra
+"""
+
+# ╔═╡ e9bbc964-d4e5-4d60-b188-68807e0a030d
+md"""
+Dividing the target wlc by each comparison star will eliminate some of the common systematics shared between them (e.g., air mass, local refractive atmospheric effects), and make the shape of the transit white-light curve more apparent.
+"""
+
+# ╔═╡ 7831034a-01fc-49bb-bd42-ff796713cc50
+md"""
+### Plot
+"""
 
 # ╔═╡ e98dee2e-a369-448e-bfe4-8fea0f318fa8
 md"""
@@ -156,15 +181,6 @@ end
 # ╔═╡ 44808b97-df11-4aff-9e97-f97987fe9939
 cube = load_npz("data/reduced_LDSS3/ut150927_flat.npy", allow_pickle = true)
 
-# ╔═╡ 5b645084-1f21-42a2-8184-e27f8b3000c3
-target_name = cube["target"]
-
-# ╔═╡ 1f3f096f-0aa3-44bc-b52b-568da4e75cf3
-comp_names = convert(Vector{String}, cube["comparisons"])
-
-# ╔═╡ 70a2863d-6f55-4cec-8c69-162157e2c199
-const ncomps = length(comp_names)
-
 # ╔═╡ 31bdc830-dfc9-445a-ba7e-76be7627561a
 wav = cube["spectral"]["wavelength"]
 
@@ -177,42 +193,79 @@ wbins_idxs = compute_wbins_idxs.(0:Δλ:Δλ*nbins-1, wbin_idx_start, Δλ)
 # ╔═╡ a0b2cff5-67cd-4ee0-b538-d39fe6c6b67c
 wav[wbins_idxs[begin][begin]], wav[wbins_idxs[end][end]]
 
+# ╔═╡ 5b645084-1f21-42a2-8184-e27f8b3000c3
+target_name, comp_names = cube["target"], convert(Vector{String}, cube["comparisons"])
+
+# ╔═╡ 70a2863d-6f55-4cec-8c69-162157e2c199
+const ncomps = length(comp_names)
+
 # ╔═╡ 639f666b-09fc-488b-982b-01a523278cae
 raw_counts = cube["cubes"]["raw_counts"]
 
 # ╔═╡ 7c6a3f88-cdc5-4b56-9bbf-2e9a2fa8ae26
 target_fluxes = raw_counts[target_name]
 
-# ╔═╡ 756c0e88-393e-404f-b36c-9f861a8172e2
-wlc_targ = sum(target_fluxes, dims=2)
-
 # ╔═╡ 6953f995-86e1-42ee-9591-8c4e372b06fb
 const ntimes = size(target_fluxes)[1]
 
 # ╔═╡ 081c0175-ea02-4a41-9dfe-60b871efc27d
 md"""
-Time index: $(@bind t_i Slider(1:10:ntimes, show_value=true))
+Time index: $(@bind t_i pl.Slider(1:10:ntimes, show_value=true))
 """
 
 # ╔═╡ bb2c6085-5cb4-4efc-a322-27fda09fb904
 comp_fluxes = cat([raw_counts[comp_names[c_i]] for c_i in 1:ncomps]..., dims=3)
 
 # ╔═╡ 2fd7bc68-a6ec-4ccb-ad73-07b031ffef5a
-begin
-	fig_targ = plot(x=wav, y=target_fluxes[t_i, :], Geom.line)
-	fig_comp_1 = plot(x=wav, y=comp_fluxes[t_i, :, 1], Geom.line)
-	fig_comp_2 = plot(x=wav, y=comp_fluxes[t_i, :, 2], Geom.line)
-	fig_comp_3 = plot(x=wav, y=comp_fluxes[t_i, :, 3], Geom.line)
-	gridstack([fig_targ fig_comp_1; fig_comp_2 fig_comp_3])
+let
+	fig = Figure(resolution = (1200, 700))
+
+	lines(fig[1, 1], wav, target_fluxes[t_i, :], label="WASP-50")
+	lines(fig[2, 1], wav, comp_fluxes[t_i, :, 1], label="comp 1")
+	lines(fig[1, 2], wav, comp_fluxes[t_i, :, 2], label="comp 2")
+	lines(fig[2, 2], wav, comp_fluxes[t_i, :, 3], label="comp 3")
+	
+	axs = reshape(copy(fig.content), 2, 2)
+	axislegend.(axs)
+	
+	linkaxes!(axs...)
+	hidexdecorations!.(axs[1, :], grid=false)
+	hideydecorations!.(axs[:, 2], grid=false)
+	
+	fig[1:2, 0] = Label(fig, "Counts", rotation=π/2)
+	fig[end+1, 2:3] = Label(fig, "Index")
+    #textsize = 30, font = noto_sans_bold, color = (:black, 0.25))
+	
+	current_figure()
 end
 
-# ╔═╡ 7eec7f0e-0faa-42bd-ae70-23d331604c7d
+# ╔═╡ 756c0e88-393e-404f-b36c-9f861a8172e2
 begin
-	p_targ = plot(wav, target_fluxes[t_i, :], label="WASP-50b")
-	p_comp_1 = plot(wav, comp_fluxes[t_i, :, 1], label="comp 1")
-	p_comp_2 = plot(wav, comp_fluxes[t_i, :, 2], label="comp 2")
-	p_comp_3 = plot(wav, comp_fluxes[t_i, :, 3], label="comp 3")
-	plot(p_targ, p_comp_1, p_comp_2, p_comp_3, layout=(2, 2), link=:all)
+	f_wlc = sum(target_fluxes, dims=2) ./ sum(comp_fluxes, dims=2)
+	f_norm_wlc = f_wlc ./ median(f_wlc, dims=1) 
+end;
+
+# ╔═╡ 76ba54ff-a396-48e2-8837-b839cb62caef
+let
+	fig = Figure(resolution = (1200, 1_000))
+	
+	for c_i in 1:ncomps
+		scatter(
+			fig[c_i, 1], vcat(f_norm_wlc[:, :, c_i]...),
+			strokewidth=0,
+			label="target / comp $(c_i)"
+		)
+	end
+	
+	axs = copy(fig.content)
+	axislegend.(axs, valign=:bottom)
+	
+	linkaxes!(axs...)
+	hidexdecorations!.(axs[1:2], grid = false)
+	axs[end].xlabel = "Index"
+	axs[2].ylabel = "Relative flux"
+	
+	current_figure()
 end
 
 # ╔═╡ a6805e63-bbf4-48bc-8e15-be24da9b0348
@@ -281,24 +334,29 @@ end
 # ╟─2d53ee50-c95b-4e9c-9ddb-e47ed9e3168b
 # ╟─1293b7f7-3054-4504-965f-2bd8ebe05e9e
 # ╟─470c738f-c2c8-4f56-b936-e08c43c161b1
+# ╟─4dddc586-2383-41f8-a888-ef421372d71a
 # ╠═44808b97-df11-4aff-9e97-f97987fe9939
 # ╟─7185f603-3e57-42db-9665-c215094776ad
-# ╠═5b645084-1f21-42a2-8184-e27f8b3000c3
-# ╠═1f3f096f-0aa3-44bc-b52b-568da4e75cf3
 # ╠═31bdc830-dfc9-445a-ba7e-76be7627561a
+# ╠═5b645084-1f21-42a2-8184-e27f8b3000c3
 # ╠═639f666b-09fc-488b-982b-01a523278cae
 # ╠═7c6a3f88-cdc5-4b56-9bbf-2e9a2fa8ae26
 # ╟─7bfc971c-8737-49ad-adec-ac57d176f10e
 # ╠═bb2c6085-5cb4-4efc-a322-27fda09fb904
 # ╟─968d1f11-12a0-4b8f-abb5-0195652e4e1f
 # ╟─e774a20f-2d58-486a-ab71-6bde678b26f8
+# ╟─ed92fff0-6413-4fc2-939e-ecd13430ce75
+# ╟─843fc32b-a0c8-4281-876a-9579a862548b
 # ╟─d6c17e99-452e-43e0-b998-10cb81009076
 # ╟─081c0175-ea02-4a41-9dfe-60b871efc27d
 # ╠═2fd7bc68-a6ec-4ccb-ad73-07b031ffef5a
-# ╠═7eec7f0e-0faa-42bd-ae70-23d331604c7d
 # ╟─e3468c61-782b-4f55-a4a1-9d1883655d11
+# ╟─08eafc08-b0fb-4c99-9d4e-7fa5085d386c
+# ╟─ae39e476-3850-4bbf-aa45-0a16c2425324
 # ╠═756c0e88-393e-404f-b36c-9f861a8172e2
-# ╠═1632c9d9-d61f-4bf6-bbfa-4d5f7deee5f3
+# ╟─e9bbc964-d4e5-4d60-b188-68807e0a030d
+# ╟─7831034a-01fc-49bb-bd42-ff796713cc50
+# ╠═76ba54ff-a396-48e2-8837-b839cb62caef
 # ╟─e98dee2e-a369-448e-bfe4-8fea0f318fa8
 # ╟─e992642a-342b-467b-ad75-594b73c88ac7
 # ╟─17039dc4-d554-4810-a147-854cd95c81d3
@@ -320,4 +378,3 @@ end
 # ╠═26f86aec-1a39-42f0-9d85-70efe0a490a3
 # ╠═3653ee36-35a6-4e0a-8d46-4f8389381d45
 # ╠═b1b0690a-a1eb-11eb-1590-396d92c80c23
-# ╠═36e5bb5f-75a1-47c3-baa3-66bd77eb9515
