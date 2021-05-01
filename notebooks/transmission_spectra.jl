5bdf26324492bf294a6bcf64677d87b8a815764d
icweaver <iweaver@cfa.harvard.edu>
2021-05-01 15:01:08 -0400

    tspec up

--- notebooks/transmission_spectra.jl
+++ notebooks/transmission_spectra.jl
@@ -5,10 +5,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 1dc0c992-a894-11eb-2eab-f1fccba239f0
-using PlutoUI, CairoMakie, Colors, CSV, DataFrames, DataFramesMeta, Glob, Measurements, Statistics
-
-# ╔═╡ 74cdebd4-4d28-42df-93a7-176d794a932e
-using Latexify
+using PlutoUI, CairoMakie, Colors, CSV, DataFrames, DataFramesMeta, Glob, Measurements, Statistics, Latexify
 
 # ╔═╡ c53be9cf-7722-4b43-928a-33e7b0463330
 const DATA_DIR = "data/detrended/out_l/WASP50"
@@ -29,7 +26,7 @@ cube = Dict(
 		) |> DataFrame
 		for (i, fpath) in enumerate(sort(glob(
 			"$(DATA_DIR)/w50_*/white-light/results.dat"
-		)))
+		)))	
 	),
 )
 
@@ -39,7 +36,8 @@ begin
 	const N_BINS = length(cube["tspec"]["Transit 1"][!, "Depth (ppm)"])
 	#wavs = [] # Can be different lengths based on different binning schemes
 	#tspecs = []
-	tspecs = Matrix{AbstractFloat}(undef, N_BINS, N_NIGHTS+1)
+	wavs = zeros(Float64,  N_BINS, N_NIGHTS)
+	tspecs = zeros(Measurement,  N_BINS, N_NIGHTS)
 	ps = Matrix{Measurement}(undef, 1, N_NIGHTS) # Rₚ/Rₛ
 	const IDX_START = 5 # Index where first common row between nights starts
 	
@@ -53,11 +51,11 @@ begin
 		#push!(wavs, wav)
 		#push!(tspecs, depth .± max_depth_err)
 		if transit == "Transit 1"
-			tspecs[:, begin] .= wav
-			tspecs[:, j+1] .= depth .± max_depth_err
+			wavs[:, j] .= wav
+			tspecs[:, j] .= depth .± max_depth_err
 		else
-			tspecs[IDX_START:end, begin] .= wav
-			tspecs[IDX_START:end, j+1] .= depth .± max_depth_err
+			wavs[IDX_START:end, j] .= wav
+			tspecs[IDX_START:end, j] .= depth .± max_depth_err
 		end
 		# Extract BMA p ≡ Rₚ/Rₛ for each night
 		symbol, p, p_u, p_d = eachcol(
@@ -72,17 +70,16 @@ begin
 	wlc_offsets = (wlc_depths .- mean_wlc_depth)
 	
 	# Extract common subset of transmission spectra
-	tspecs_common = tspecs[IDX_START:end, begin+1:end]
+	tspecs_common = tspecs[IDX_START:end, :]
 	
 	# Combine
 	tspecs_wlc_adj = @. tspecs_common - wlc_offsets
 	tspec_combined = mapslices(weightedmean, tspecs_wlc_adj, dims=2)
-end
+end;
 
 # ╔═╡ 8373a47f-9596-43f2-b5c8-bb179de3eec8
 begin
-	wav_common = tspecs[IDX_START:end, begin]
-	
+	wav_common = wavs[IDX_START:end, end]
 	df_wav = DataFrame([wav_common], [:Wavelength])
 	
 	df_tspecs = DataFrame(
@@ -90,51 +87,14 @@ begin
 		[keys(cube["tspec"])..., "Combined"]
 	)
 	
-	hcat(df_wav, df_tspecs)
+	hcat(df_wav, df_tspecs) |> latexify
 end
 
-# ╔═╡ 5c6d5b2d-3521-4626-b12d-d5685e0b09a5
-# begin
-# 	fig = Figure()
-	
-# 	ax = Axis(fig[1, 1], xlabel="Wavelength (Å)", ylabel="Relative depth")
-	
-# 	hlines!(ax, 0, color=:grey, linestyle=:dash)
-	
-# 	for (i, tspec) in enumerate(eachcol(tspecs_wlc_adj))
-# 		depth = Measurements.value.(tspec)
-# 		depth_err = Measurements.uncertainty.(tspec)
-# 		errorbars!(ax, wav_common, depth .- mean_wlc_depth.val, depth_err;
-# 			color = COLORS[i],
-# 			whiskerwidth = 0,
-# 			linewidth = 3,
-# 		)
-# 		scatter!(ax, wav_common, depth .- mean_wlc_depth.val;
-# 			color = COLORS[i],
-# 			strokewidth = 0,
-# 			markersize = 12,
-# 			label = "Transit $i",
-# 		)
-# 	end
+# ╔═╡ 64fd6b14-028f-4396-8a6d-240c73896174
+Measurements.uncertainty.(tspec_combined) |> mean
 
-# 	tspec_combined = mapslices(weightedmean, tspecs_common .- offsets, dims=2)
-# 	f_combined = Measurements.value.(tspec_combined) |> vec
-# 	f_combined_err = Measurements.uncertainty.(tspec_combined) |> vec
-
-# 	wavelengths = cube["tspec"]["Transit 3"][!, "Wav_d"]
-# 	errorbars!(ax, wavelengths, f_combined .- mean_wlc_depth.val, f_combined_err;
-# 		linewidth = 3,
-# 	)
-# 	scatter!(ax, wavelengths, f_combined .- mean_wlc_depth.val;
-# 		color = :white,
-# 		strokewidth = 2,
-# 		label = "Combined",
-# 	)
-
-# 	axislegend(orientation=:horizontal, valign=:bottom, labelsize=16)
-		
-# 	fig
-# end
+# ╔═╡ 817aac12-13b9-44fe-a7e5-aa5ec7b8939e
+mean_wlc_depth
 
 # ╔═╡ bef0918c-c645-4557-a2e5-00b6c26573bc
 const COLORS =  parse.(Colorant,
@@ -151,12 +111,61 @@ const COLORS =  parse.(Colorant,
 	]
 );
 
+# ╔═╡ 5c6d5b2d-3521-4626-b12d-d5685e0b09a5
+begin
+	fig = Figure()
+	
+	ax = Axis(fig[1, 1], xlabel="Wavelength (Å)", ylabel="Relative depth")
+	
+	hlines!(ax, 0, color=:grey, linestyle=:dash)
+	
+	for (i, tspec) in enumerate(eachcol(tspecs_wlc_adj))
+		depth = Measurements.value.(tspec)
+		depth_err = Measurements.uncertainty.(tspec)
+		errorbars!(ax, wav_common, depth .- mean_wlc_depth.val, depth_err;
+			color = COLORS[i],
+			whiskerwidth = 0,
+			linewidth = 3,
+		)
+		scatter!(ax, wav_common, depth .- mean_wlc_depth.val;
+			color = COLORS[i],
+			strokewidth = 0,
+			markersize = 12,
+			label = "Transit $i",
+		)
+	end
+
+	f_combined = Measurements.value.(tspec_combined) |> vec
+	f_combined_err = Measurements.uncertainty.(tspec_combined) |> vec
+
+	errorbars!(ax, wav_common, f_combined .- mean_wlc_depth.val, f_combined_err;
+		linewidth = 3,
+	)
+	scatter!(ax, wav_common, f_combined .- mean_wlc_depth.val;
+		color = :white,
+		strokewidth = 2,
+		label = "Combined",
+	)
+	
+	# Plot blue points from Transit 1
+	wav_blue = wavs[begin:IDX_START-1, begin] |> vec
+	f_blue = Measurements.value.(tspecs[begin:IDX_START-1, begin]) |> vec
+	f_blue_err = Measurements.uncertainty.(tspecs[begin:IDX_START-1, begin]) |> vec
+	errorbars!(ax, wav_blue, f_blue .- mean_wlc_depth.val, f_blue_err)
+	scatter!(ax, wav_blue, f_blue .- mean_wlc_depth.val, strokewidth=0)
+
+	axislegend(orientation=:horizontal, valign=:bottom, labelsize=16)
+		
+	fig
+end
+
 # ╔═╡ Cell order:
-# ╠═1dc0c992-a894-11eb-2eab-f1fccba239f0
 # ╠═c53be9cf-7722-4b43-928a-33e7b0463330
 # ╠═5c4fcb25-9a26-43f1-838b-338b33fb9ee6
 # ╠═855095aa-c7e1-4799-9fcb-070a95bf7656
 # ╠═8373a47f-9596-43f2-b5c8-bb179de3eec8
-# ╠═74cdebd4-4d28-42df-93a7-176d794a932e
+# ╠═64fd6b14-028f-4396-8a6d-240c73896174
+# ╠═817aac12-13b9-44fe-a7e5-aa5ec7b8939e
 # ╠═5c6d5b2d-3521-4626-b12d-d5685e0b09a5
 # ╠═bef0918c-c645-4557-a2e5-00b6c26573bc
+# ╠═1dc0c992-a894-11eb-2eab-f1fccba239f0
