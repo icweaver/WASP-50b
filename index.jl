d24366173e2decdc34a1b4b6d665c73d37157509
icweaver <iweaver@cfa.harvard.edu>
2021-06-11 14:13:25 -0400

    added index

---
+++ index.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.14.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 906a22a4-cae0-11eb-009d-7d4c76978af6
+md"""
+# Index page for WASP-50b
+"""
+
+# ╔═╡ Cell order:
+# ╟─906a22a4-cae0-11eb-009d-7d4c76978af6
