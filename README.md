9cdb772f8fd82b533aef843bc17822e9b7bc724f
icweaver <iweaver@cfa.harvard.edu>
2021-07-02 17:45:09 -0400

    updated links

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # WASP-50b
 
-1. [Raw Frames](https://icweaver.github.io/WASP-50b/html/01_raw_frames.html)
-1. [Reduced Data -- IMACS](https://icweaver.github.io/WASP-50b/html/02_reduced.html)
-1. [Reduced Data -- LDSS3](https://icweaver.github.io/WASP-50b/html/03_reduced_LDSS3.html)
-1. [Detrended White Light Curves](https://icweaver.github.io/WASP-50b/html/04_detrended_wlcs.html)
-1. [Detrended Binned Light Curves](https://icweaver.github.io/WASP-50b/html/05_detrended_bwlcs.html)
-1. [Photometric Monitoring](https://icweaver.github.io/WASP-50b/html/06_photometric_monitoring.html)
-1. [Transmission Spectra](https://icweaver.github.io/WASP-50b/html/07_transmission_spectra.html)
+1. [Raw Frames](https://icweaver.github.io/WASP-50b/html/01_raw_frames.jl.html)
+1. [Reduced Data -- IMACS](https://icweaver.github.io/WASP-50b/html/02_reduced.jl.html)
+1. [Reduced Data -- LDSS3](https://icweaver.github.io/WASP-50b/html/03_reduced_LDSS3.jl.html)
+1. [Detrended White Light Curves](https://icweaver.github.io/WASP-50b/html/04_detrended_wlcs.jl.html)
+1. [Detrended Binned Light Curves](https://icweaver.github.io/WASP-50b/html/05_detrended_bwlcs.jl.html)
+1. [Photometric Monitoring](https://icweaver.github.io/WASP-50b/html/06_photometric_monitoring.jl.html)
+1. [Transmission Spectra](https://icweaver.github.io/WASP-50b/html/07_transmission_spectra.jl.html)
--- index.jl
+++ index.jl
@@ -6,28 +6,16 @@ using InteractiveUtils
 
 # ╔═╡ 906a22a4-cae0-11eb-009d-7d4c76978af6
 md"""
-# Index page for WASP-50b
+# WASP-50b
 
-1. [Raw Frames](https://icweaver.github.io/WASP-50b/html/01_raw_frames.html)
-1. [Reduced Data -- IMACS](https://icweaver.github.io/WASP-50b/html/02_reduced.html)
-1. [Reduced Data -- LDSS3](https://icweaver.github.io/WASP-50b/html/03_reduced_LDSS3.html)
-1. [Detrended White Light Curves](https://icweaver.github.io/WASP-50b/html/04_detrended_wlcs.html)
-1. [Detrended Binned Light Curves](https://icweaver.github.io/WASP-50b/html/05_detrended_bwlcs.html)
-1. [Photometric Monitoring](https://icweaver.github.io/WASP-50b/html/06_photometric_monitoring.html)
-1. [Transmission Spectra](https://icweaver.github.io/WASP-50b/html/07_transmission_spectra.html)
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
-
+1. [Raw Frames](https://icweaver.github.io/WASP-50b/html/01_raw_frames.jl.html)
+1. [Reduced Data -- IMACS](https://icweaver.github.io/WASP-50b/html/02_reduced.jl.html)
+1. [Reduced Data -- LDSS3](https://icweaver.github.io/WASP-50b/html/03_reduced_LDSS3.jl.html)
+1. [Detrended White Light Curves](https://icweaver.github.io/WASP-50b/html/04_detrended_wlcs.jl.html)
+1. [Detrended Binned Light Curves](https://icweaver.github.io/WASP-50b/html/05_detrended_bwlcs.jl.html)
+1. [Photometric Monitoring](https://icweaver.github.io/WASP-50b/html/06_photometric_monitoring.jl.html)
+1. [Transmission Spectra](https://icweaver.github.io/WASP-50b/html/07_transmission_spectra.jl.html)
 """
 
 # ╔═╡ Cell order:
 # ╟─906a22a4-cae0-11eb-009d-7d4c76978af6
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
