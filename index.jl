8f644af68465d7dd18203878f446015df96fcc3d
icweaver <iweaver@cfa.harvard.edu>
2021-07-23 14:50:35 -0400

    updated index

--- index.jl
+++ index.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.15.0
+# v0.15.1
 
 using Markdown
 using InteractiveUtils
@@ -8,13 +8,7 @@ using InteractiveUtils
 md"""
 # WASP-50b
 
-1. [Raw Frames](https://icweaver.github.io/WASP-50b/html/01_raw_frames.jl.html)
-1. [Reduced Data -- IMACS](https://icweaver.github.io/WASP-50b/html/02_reduced.jl.html)
-1. [Reduced Data -- LDSS3](https://icweaver.github.io/WASP-50b/html/03_reduced_LDSS3.jl.html)
-1. [Detrended White Light Curves](https://icweaver.github.io/WASP-50b/html/04_detrended_wlcs.jl.html)
-1. [Detrended Binned Light Curves](https://icweaver.github.io/WASP-50b/html/05_detrended_bwlcs.jl.html)
-1. [Photometric Monitoring](https://icweaver.github.io/WASP-50b/html/06_photometric_monitoring.jl.html)
-1. [Transmission Spectra](https://icweaver.github.io/WASP-50b/html/07_transmission_spectra.jl.html)
+Site under construction. Navigate to README [here](https://github.com/icweaver/WASP-50b#readme) for now.
 """
 
 # ╔═╡ Cell order:
