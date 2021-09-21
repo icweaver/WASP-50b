4fc0041e54cc987a3310c96660beabac8f7bace4
icweaver <iweaver@cfa.harvard.edu>
2021-09-21 15:33:47 -0400

    up

--- notebooks/pop.jl
+++ notebooks/pop.jl
@@ -46,12 +46,6 @@ df_all = let
 	CSV.read(request.body, DataFrame)
 end
 
-# ╔═╡ 9aed232f-ec74-4ec6-9ae7-06b90539833b
-df = @chain df_all begin
-	dropmissing([:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag, :pl_trandep])
-	#@subset(:pl_rade .≤ 11.0)
-end
-
 # ╔═╡ 2776646e-47e7-4b9e-ab91-4035bc6df99f
 function compute_scale_factor(Rp)
 	if Rp < 1.5
@@ -71,18 +65,34 @@ end
 # ╔═╡ c7960066-cc33-480c-807b-c56ead4262bf
 function compute_TSM(Rp, Teq, Mp, Rs, J; denom=1.0)
 	f = compute_scale_factor(Rp)
-	#f = 1.0
 	return f * (Rp^3 * Teq / (Mp * Rs^2)) * 10.0^(-J/5.0) / denom
 end
 
-# ╔═╡ de1cc207-ccc9-41ba-bd73-e1f912b65a04
-df_candidates = @chain df begin
+# ╔═╡ 2a59198c-95aa-4360-be05-49b38f1c9171
+const G = ustrip(u"m*Rearth^2/Mearth/s^2", Unitful.G)
+
+# ╔═╡ 46942e93-fa05-48ce-9639-248ccb63fa30
+compute_g(Mp, Rp) = G * Mp / Rp^2
+
+# ╔═╡ 9aed232f-ec74-4ec6-9ae7-06b90539833b
+df = @chain df_all begin
+	dropmissing([:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag, :pl_trandep])
+	@transform :pl_g = compute_g.(:pl_bmasse, :pl_rade)
+	@transform :pl_TSM = compute_TSM.(
+		:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag
+	)
 	@subset @. (1.0 ≤ :pl_TSMR ≤ 20.0) & (20.0 ≤ :pl_g ≤ 100.0)
 	sort(:pl_TSMR, rev=true)
 end
 
+# ╔═╡ eebe1f83-d24c-4c42-aa98-00c7a9049ce7
+TSM₀ = @subset(df, :pl_name .== "HAT-P-23 b").pl_TSM[1]
+
+# ╔═╡ 40c20156-18bc-4e57-bfd2-96f9432533b4
+df.pl_TSMR = df.pl_TSM ./ TSM₀
+
 # ╔═╡ d62b5506-1411-49f2-afe3-d4aec70641a1
-df_HGHJs = @subset(df_candidates, :pl_name .∈ Ref(["HAT-P-23 b", "WASP-43 b", "WASP-50 b"]))
+df_HGHJs = @subset(df, :pl_name .∈ Ref(["HAT-P-23 b", "WASP-43 b", "WASP-50 b"]))
 
 # ╔═╡ c1cd9292-28b9-4206-b128-608aaf30ff9c
 let
@@ -94,39 +104,15 @@ let
 	)
 	marker_open = visual(marker='○')
 	marker_closed = visual(marker='●')
-	plt = m * (data(df_candidates)*marker_open + data(df_HGHJs)*marker_closed)
+	plt = m * (data(df)*marker_open + data(df_HGHJs)*marker_closed)
 	fg = draw(plt)
 	
 	ax = fg.grid[1, 1].axis
 	vlines!(ax, 30.0, color=:darkgrey, linestyle=:dash)
-	TSMR_range = round.(extrema(df_candidates.pl_TSMR))
+	TSMR_range = round.(extrema(df.pl_TSMR))
 	text!(ax, "TSMR: $(TSMR_range[1]) -- $(TSMR_range[2])", position=(35, 2_500))
 	
 	fg
-end
-
-# ╔═╡ 2a59198c-95aa-4360-be05-49b38f1c9171
-const G = ustrip(u"m*Rearth^2/Mearth/s^2", Unitful.G)
-
-# ╔═╡ 46942e93-fa05-48ce-9639-248ccb63fa30
-compute_g(Mp, Rp) = G * Mp / Rp^2
-
-# ╔═╡ 7b52e0aa-cf3e-472e-9ca6-06db018ac86d
-begin
-	@transform! df begin
-		:pl_g = compute_g.(:pl_bmasse, :pl_rade)
-		:pl_TSM = compute_TSM.(
-			:pl_rade,
-			:pl_eqt,
-			:pl_bmasse,
-			:st_rad,
-			:sy_jmag,
-		)
-	end
-	
-	TSM₀ = @subset(df, :pl_name .== "HAT-P-23 b").pl_TSM[1]
-	
-	df.pl_TSMR = df.pl_TSM ./ TSM₀
 end
 
 # ╔═╡ dcb16743-4ea5-45e8-936e-c6445bfa010f
@@ -150,12 +136,12 @@ end
 # ╟─c64b8c73-786c-451b-b8fc-43ee5ded5fcd
 # ╠═f396cda3-f535-4ad9-b771-7ccbd45c54f3
 # ╠═9aed232f-ec74-4ec6-9ae7-06b90539833b
+# ╠═eebe1f83-d24c-4c42-aa98-00c7a9049ce7
+# ╠═40c20156-18bc-4e57-bfd2-96f9432533b4
+# ╠═d62b5506-1411-49f2-afe3-d4aec70641a1
 # ╠═2776646e-47e7-4b9e-ab91-4035bc6df99f
 # ╠═46942e93-fa05-48ce-9639-248ccb63fa30
-# ╠═7b52e0aa-cf3e-472e-9ca6-06db018ac86d
 # ╠═c7960066-cc33-480c-807b-c56ead4262bf
-# ╠═de1cc207-ccc9-41ba-bd73-e1f912b65a04
-# ╠═d62b5506-1411-49f2-afe3-d4aec70641a1
 # ╠═c1cd9292-28b9-4206-b128-608aaf30ff9c
 # ╠═2a59198c-95aa-4360-be05-49b38f1c9171
 # ╠═dcb16743-4ea5-45e8-936e-c6445bfa010f
