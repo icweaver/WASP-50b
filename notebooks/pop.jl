189b13b7799f14ccac1f2dcf3bf92b15d8ae95ee
icweaver <iweaver@cfa.harvard.edu>
2021-09-22 19:51:11 -0400

    units up

--- notebooks/pop.jl
+++ notebooks/pop.jl
@@ -14,7 +14,7 @@ begin
 	using HTTP
 	using Unitful
 	using PlutoUI
-	using Unitful: k, σ, G
+	using Unitful: k, G
 end
 
 # ╔═╡ cd13d7f3-0ea3-4631-afd9-5f3e359000e6
@@ -66,6 +66,29 @@ md"""
 We next compute some relevant quantities from this table to help organize each population:
 """
 
+# ╔═╡ 9aed232f-ec74-4ec6-9ae7-06b90539833b
+# function make_table1()
+# 	df = @chain df_all begin
+# 		dropmissing([:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag, :pl_trandep])
+# 		@aside g_SI = let
+# 			g = compute_g.(_.pl_bmasse*u"Mearth", _.pl_rade*u"Rearth")
+# 			ustrip.(u"m/s^2", g)
+# 		end
+# 		@aside H_km = let
+# 			H = compute_H.(2.0*u"u", _.pl_eqt*u"K", g_SI*u"m/s^2")
+# 			ustrip.(u"km", H)
+# 		end
+# 		@transform begin
+# 			:g_SI = g_SI
+# 			:H_km = H_km
+# 			:TSM = compute_TSM.(
+# 				:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag
+# 			)
+# 		end
+# 		@transform :TSMR = :TSM ./ :TSM[:pl_name .== "HAT-P-23 b"][1]
+# 	end
+# end
+
 # ╔═╡ 4cbbb1e8-e5fb-4ab0-a7e6-7881c2dde032
 md"""
 With this list of $(nrow(df_all)) transiting exoplanets with observed stellar and planetary parameters, we now turn to visualizing the subset of each population from the list above.
@@ -100,29 +123,6 @@ function compute_scale_factor(Rp)
 	return f
 end
 
-# ╔═╡ 46942e93-fa05-48ce-9639-248ccb63fa30
-# Compute g, in m/s², assuming M, R in Earth units
-compute_ge_SI(Mpe, Rpe) = Ge * Mpe / Rpe^2
-
-# ╔═╡ 1e587a84-ed43-4fac-81cf-134a4f3d65d5
-compute_H(μ, T, g) = k * T / μ #(μ * g) # For testing units
-
-# ╔═╡ 09e280b6-7e1b-4565-9aaf-034d3420140c
-compute_H(2u"u", 1_000u"K", 30u"m/s^2")# |> u"km"
-
-# ╔═╡ 32a33cc4-e5a4-4217-8ce9-180c9d19d13f
-const k_SI = ustrip(k) # Already in SI
-
-# ╔═╡ 7056d308-670f-4d55-98a8-1be8192c1ad8
-# Compute H
-compute_H_SI(μ, T, g) = k_SI * T / μ #(μ * g)
-
-# ╔═╡ f6bbfc30-21d4-4975-baf3-950af588337d
-compute_H_SI(2, 1_000, 30)
-
-# ╔═╡ 8fe94715-722b-4a3d-a9bc-4ea2fe4cc4db
-u_SI = ustrip(u"kg", 1u"u")
-
 # ╔═╡ c7960066-cc33-480c-807b-c56ead4262bf
 # compute TSM, assuming M, R in Earth units, T in Kelvin
 function compute_TSM(Rp, Teq, Mp, Rs, J; denom=1.0)
@@ -130,26 +130,41 @@ function compute_TSM(Rp, Teq, Mp, Rs, J; denom=1.0)
 	return f * (Rp^3 * Teq / (Mp * Rs^2)) * 10.0^(-J/5.0) / denom
 end
 
-# ╔═╡ 9aed232f-ec74-4ec6-9ae7-06b90539833b
+# ╔═╡ abaee9cc-9841-4b6b-ad33-2093c27422c8
+compute_g(M, R) = G * M / R^2
+
+# ╔═╡ 1e587a84-ed43-4fac-81cf-134a4f3d65d5
+compute_H(μ, T, g) = k * T / (μ * g)
+
+# ╔═╡ c1e63cf3-7f30-4858-bdd6-125d2a99529f
+compute_ΔD(H, Rₚ, Rₛ) = 2.0 * H * Rₚ/Rₛ^2
+
+# ╔═╡ 2702ba80-993c-4cca-bd14-0d4d6b67362b
 begin
-	df = @chain df_all begin
-		dropmissing([:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag, :pl_trandep])
-		@transform begin
-			:pl_g = compute_g.(:pl_bmasse, :pl_rade)
-			:pl_TSM = compute_TSM.(
-				:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag
-			)
-		end
+	df = dropmissing(
+		df_all, [:pl_rade, :pl_eqt, :pl_bmasse, :st_rad, :sy_jmag, :pl_trandep]
+	)
+	df.g_SI = let
+		g = compute_g.(df.pl_bmasse*u"Mearth", df.pl_rade*u"Rearth")
+		ustrip.(u"m/s^2", g)
 	end
-	TSM0 = df.pl_TSM[df.pl_name .== "HAT-P-23 b"][1]
-	df.pl_TSMR = df.pl_TSM / TSM0
+	df.H_km = let
+		H = compute_H.(2.0*u"u", df.pl_eqt*u"K", df.g_SI*u"m/s^2")
+		ustrip.(u"km", H)
+	end
+	df.TSM = compute_TSM.(df.pl_rade, df.pl_eqt, df.pl_bmasse, df.st_rad, df.sy_jmag)
+	df.TSMR = df.TSM ./ df.TSM[df.pl_name .== "HAT-P-23 b"][1]
+	df.ΔD = let
+		ΔD = compute_ΔD.(df.H_km*u"km", df.pl_rade*u"Rearth", df.st_rad*u"Rsun")
+		uconvert.(NoUnits, ΔD) * 5.0 * 1e6
+	end
 	df
 end
 
 # ╔═╡ c98c5618-4bac-4322-b4c3-c76181f47889
 df_HGHJs_all = @chain df begin
-	@subset @. (TSM_low ≤ :pl_TSMR ≤ TSM_high) & (g_low ≤ :pl_g ≤ g_high)
-	sort(:pl_TSMR, rev=true)
+	@subset @. (TSM_low ≤ :TSMR ≤ TSM_high) & (g_low ≤ :g_SI ≤ g_high)
+	sort(:TSMR, rev=true)
 end
 
 # ╔═╡ d62b5506-1411-49f2-afe3-d4aec70641a1
@@ -161,9 +176,9 @@ df_HGHJs = @subset(
 let
 	m = mapping(
 		:pl_eqt => "Equilibrium temperature (K)",
-		:pl_g => "Surface gravity (m/s²)",
-		color = :sy_tmag => "Tess magnitude",
-		markersize = :pl_TSMR => (x -> 15.0*x),
+		:g_SI => "Surface gravity (m/s²)",
+		color = :ΔD => "ΔD (ppm)",
+		markersize = :TSMR => (x -> 15.0*x),
 	)
 	marker_open = visual(marker='○')
 	marker_closed = visual(marker='●')
@@ -172,14 +187,17 @@ let
 	
 	ax = fg.grid[1, 1].axis
 	hlines!(ax, 30.0, color=:darkgrey, linestyle=:dash)
-	TSMR_range = round.(extrema(df.pl_TSMR))
+	TSMR_range = round.(extrema(df.TSMR))
 	text!(ax, "TSMR: $(TSMR_range[1]) – $(TSMR_range[2])", position=(450, 45))
 	
 	fg
 end
 
-# ╔═╡ 2a59198c-95aa-4360-be05-49b38f1c9171
-const Ge_SI = ustrip(u"m*Rearth^2/Mearth/s^2", G)
+# ╔═╡ 4084831d-e357-430a-ab43-7d9dd494d6ed
+(2 * 126u"km" * 1.036u"Rjup" / 0.667u"Rsun"^2) |> x -> uconvert(NoUnits, x)
+
+# ╔═╡ eeebf896-5832-46ba-a004-20b0307df97c
+(2 * 126u"km" * 0.1596 / 0.667u"Rsun") |> ustrip 
 
 # ╔═╡ dcb16743-4ea5-45e8-936e-c6445bfa010f
 begin
@@ -198,27 +216,12 @@ begin
 	COLORS = Makie.wong_colors()
 end
 
-# ╔═╡ 4bd35a25-5e17-4b71-9f42-5bb5e6caa53c
-let
-	#df = DataFrameMacros.titanic()
-	
-	df0 = DataFrame(
-		:name => ["apple", "banana", "watermelon"],
-		:x =>[1, 2, 3],
-		:y => [2, 4, 6],
-	)
-	
-	df = @transform df0 begin
-		:z = :x .* :y
-		:w = :x
-	end
-end
-
 # ╔═╡ Cell order:
 # ╟─cd13d7f3-0ea3-4631-afd9-5f3e359000e6
 # ╟─6b06701b-05e2-4284-a308-e9edeb65a648
 # ╠═f396cda3-f535-4ad9-b771-7ccbd45c54f3
 # ╟─4d1a7740-24c7-4cec-b788-a386bc25f836
+# ╠═2702ba80-993c-4cca-bd14-0d4d6b67362b
 # ╠═9aed232f-ec74-4ec6-9ae7-06b90539833b
 # ╟─4cbbb1e8-e5fb-4ab0-a7e6-7881c2dde032
 # ╟─7f956b36-ce65-4e4e-afa5-9b97b9e06954
@@ -229,14 +232,10 @@ end
 # ╠═d62b5506-1411-49f2-afe3-d4aec70641a1
 # ╠═2776646e-47e7-4b9e-ab91-4035bc6df99f
 # ╠═c7960066-cc33-480c-807b-c56ead4262bf
-# ╠═46942e93-fa05-48ce-9639-248ccb63fa30
-# ╠═7056d308-670f-4d55-98a8-1be8192c1ad8
+# ╠═abaee9cc-9841-4b6b-ad33-2093c27422c8
 # ╠═1e587a84-ed43-4fac-81cf-134a4f3d65d5
-# ╠═09e280b6-7e1b-4565-9aaf-034d3420140c
-# ╠═f6bbfc30-21d4-4975-baf3-950af588337d
-# ╠═32a33cc4-e5a4-4217-8ce9-180c9d19d13f
-# ╠═8fe94715-722b-4a3d-a9bc-4ea2fe4cc4db
-# ╠═2a59198c-95aa-4360-be05-49b38f1c9171
+# ╠═c1e63cf3-7f30-4858-bdd6-125d2a99529f
+# ╠═4084831d-e357-430a-ab43-7d9dd494d6ed
+# ╠═eeebf896-5832-46ba-a004-20b0307df97c
 # ╠═dcb16743-4ea5-45e8-936e-c6445bfa010f
 # ╠═24c6a2d0-0aea-11ec-2cd4-3de7ec08b83e
-# ╠═4bd35a25-5e17-4b71-9f42-5bb5e6caa53c
